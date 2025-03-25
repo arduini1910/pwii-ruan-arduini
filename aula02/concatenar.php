1dc2c7bd04584878c85e37ef50e43e50fcda0f64
Ruan Arduini <arduiniruan@gmail.com>
2025-03-25 16:36:25 -0300

    aula 02 sobre concatenação

---
+++ aula02/concatenar.php
@@ -0,0 +1,14 @@
+<?php
+ echo "hello world!<br>";
+ echo "hello world!<br>";
+ echo "hello world!<br>";
+
+ $nome = "Ruan";
+ $sobrenome = "Arduini";
+ $numero = 5; 
+
+ echo $nome . "lindo" . "<br>";
+ echo "${nome} <h1> ${sobrenome} </h1>";
+ echo $numero . "<p>" . $nome .
+ " " . $sobrenome . "</p>";
+ ?>
